cf559569258850a247aaaca7ad479b978dd2ca41
Go <go05sato@gmail.com>
2018-09-03 14:02:12 +0900

    WebGLビルドの時にはMicrophoneを使用しないようにdefineを設定

--- DroneCameraController/Assets/NatCorder/Examples/ReplayCam/ReplayCam.cs
+++ DroneCameraController/Assets/NatCorder/Examples/ReplayCam/ReplayCam.cs
@@ -3,7 +3,8 @@
 *   Copyright (c) 2018 Yusuf Olokoba
 */
 
-namespace NatCorderU.Examples {
+namespace NatCorderU.Examples
+{
 
     using UnityEngine;
     using UnityEngine.UI;
@@ -11,7 +12,8 @@ namespace NatCorderU.Examples {
     using Core;
     using Core.Recorders;
 
-    public class ReplayCam : MonoBehaviour {
+    public class ReplayCam : MonoBehaviour
+    {
 
         /**
         * ReplayCam Example
@@ -32,7 +34,8 @@ namespace NatCorderU.Examples {
         private CameraRecorder videoRecorder;
         private AudioRecorder audioRecorder;
 
-        public void StartRecording () {
+        public void StartRecording()
+        {
             // First make sure recording microphone is only on MP4
             recordMicrophone &= container == Container.MP4;
             // Create recording configurations // Clamp video width to 720
@@ -40,7 +43,7 @@ namespace NatCorderU.Examples {
             var height = width * Screen.height / Screen.width;
             var framerate = container == Container.GIF ? 10 : 30;
             var videoFormat = new VideoFormat(width, (int)height, framerate);
-            var audioFormat = recordMicrophone ? AudioFormat.Unity: AudioFormat.None;
+            var audioFormat = recordMicrophone ? AudioFormat.Unity : AudioFormat.None;
             // Start recording
             NatCorder.StartRecording(container, videoFormat, audioFormat, OnReplay);
             videoRecorder = CameraRecorder.Create(Camera.main);
@@ -48,14 +51,16 @@ namespace NatCorderU.Examples {
             if (container == Container.GIF)
                 videoRecorder.recordEveryNthFrame = 5;
             // Start microphone and create audio recorder
-            if (recordMicrophone) {
+            if (recordMicrophone)
+            {
                 StartMicrophone();
                 audioRecorder = AudioRecorder.Create(microphoneSource, true);
             }
         }
 
-        private void StartMicrophone () {
-            #if !UNITY_WEBGL || UNITY_EDITOR // No `Microphone` API on WebGL :(
+        private void StartMicrophone()
+        {
+#if !UNITY_WEBGL || UNITY_EDITOR // No `Microphone` API on WebGL :(
             // Create a microphone clip
             microphoneSource.clip = Microphone.Start(null, true, 60, 48000);
             while (Microphone.GetPosition(null) <= 0) ;
@@ -63,29 +68,34 @@ namespace NatCorderU.Examples {
             microphoneSource.timeSamples = Microphone.GetPosition(null);
             microphoneSource.loop = true;
             microphoneSource.Play();
-            #endif
+#endif
         }
 
-        public void StopRecording () {
+        public void StopRecording()
+        {
             // Stop the microphone if we used it for recording
-            if (recordMicrophone) {
+#if !UNITY_WEBGL || UNITY_EDITOR
+            if (recordMicrophone)
+            {
                 Microphone.End(null);
                 microphoneSource.Stop();
                 audioRecorder.Dispose();
             }
+#endif
             // Stop the recording
             videoRecorder.Dispose();
             NatCorder.StopRecording();
         }
 
-        void OnReplay (string path) {
-            Debug.Log("Saved recording to: "+path);
+        void OnReplay(string path)
+        {
+            Debug.Log("Saved recording to: " + path);
             // Playback the video
-            #if UNITY_IOS
+#if UNITY_IOS
             Handheld.PlayFullScreenMovie("file://" + path);
-            #elif UNITY_ANDROID
+#elif UNITY_ANDROID
             Handheld.PlayFullScreenMovie(path);
-            #endif
+#endif
         }
     }
 }
