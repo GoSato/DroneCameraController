d10f246847666d130c8654b3ac810c14afaf4507
Go <go05sato@gmail.com>
2018-09-14 13:58:44 +0900

    Audio込みで録画できるようにUnity用ネイティブプラグイン実装

---
+++ DroneCameraController/Assets/TestLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class TestLoader : MonoBehaviour
+{
+    [DllImport("__Internal")]
+    private static extern void LoadRecorder();
+
+    public void CallLoadRecorder()
+    {
+        Debug.Log("OnClick");
+        LoadRecorder();
+    }
+}
