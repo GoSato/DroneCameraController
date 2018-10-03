b5489bb9c0455901c44b0e737ab9e155e0010e89
go <go@psychic-vr-lab.com>
2018-10-04 04:12:55 +0900

    リネーム

---
+++ DroneCameraController/Assets/STYLY_CameraController/STYLY_CameraControllerManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum CameraControllerMode
+{
+    Manual,
+    AutoOrbit,
+}
+
+public class STYLY_CameraControllerManager : MonoBehaviour
+{
+    [SerializeField]
+    private STYLY_ManualCameraControllerUI _manualCameraControllerUI;
+
+    [SerializeField]
+    private STYLY_AutoOrbitCameraControllerUI _autoOrbitCameraControllerUI;
+
+    private CameraControllerMode _curretCameraControllerMode = CameraControllerMode.Manual;
+
+    private void Start()
+    {
+        _curretCameraControllerMode = CameraControllerMode.AutoOrbit;
+        OpenOrCloseCameraControllerUI(false);
+        _curretCameraControllerMode = CameraControllerMode.Manual;
+        OpenOrCloseCameraControllerUI(true);
+        OpenOrCloseCameraControllerUI(false, true);
+    }
+
+    public void OnOpenManualCameraControllerUI()
+    {
+        OpenOrCloseCameraControllerUI(false);
+        _curretCameraControllerMode = CameraControllerMode.Manual;
+        OpenOrCloseCameraControllerUI(true);
+    }
+
+    public void OnOpenAutoOrbitCameraControllerUI()
+    {
+        OpenOrCloseCameraControllerUI(false);
+        _curretCameraControllerMode = CameraControllerMode.AutoOrbit;
+        OpenOrCloseCameraControllerUI(true);
+    }
+
+    public void OnOpenOrCloseCameraControllerUI(Toggle toggle)
+    {
+        bool active = toggle.isOn;
+
+        OpenOrCloseCameraControllerUI(active, true);
+    }
+
+    private void OpenOrCloseCameraControllerUI(bool active, bool onlyUI = false)
+    {
+        switch (_curretCameraControllerMode)
+        {
+            case CameraControllerMode.Manual:
+                _manualCameraControllerUI.SetActive(active, onlyUI);
+                break;
+            case CameraControllerMode.AutoOrbit:
+                _autoOrbitCameraControllerUI.SetActive(active, onlyUI);
+                break;
+        }
+    }
+}
---
+++ DroneCameraController/Assets/STYLY_CameraController/STYLY_ManualCameraControllerUI.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class STYLY_ManualCameraControllerUI : MonoBehaviour
+{
+    [SerializeField]
+    private ManualCameraController _manualCameraController;
+
+    private CanvasGroup _canvasGroup;
+    public CanvasGroup CanvasGroup
+    {
+        get
+        {
+            if (_canvasGroup == null)
+            {
+                _canvasGroup = GetComponent<CanvasGroup>();
+            }
+            return _canvasGroup;
+        }
+    }
+
+    public void SetActive(bool active, bool onlyUI = false)
+    {
+        if (!onlyUI)
+        {
+            _manualCameraController.SetActive(active);
+        }
+
+        gameObject.SetActive(active);
+        CanvasGroup.alpha = active ? 1 : 0;
+        CanvasGroup.interactable = active;
+        CanvasGroup.blocksRaycasts = active;
+    }
+
+    public void OnSetAxisMode(Dropdown dropdown)
+    {
+        ManualCameraController.AxisMode _axisMode;
+
+        switch(dropdown.value)
+        {
+            case 0:
+            default:
+                _axisMode = ManualCameraController.AxisMode.Local;
+                break;
+            case 1:
+                _axisMode = ManualCameraController.AxisMode.Global;
+                break;
+        }
+
+        _manualCameraController.SetAxisMode(_axisMode);
+    }
+}
